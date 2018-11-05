b69e994ecbec303d9ace5d13d5ca3266fc4de87f
Alejandro Castillo <alexcc134@gmail.com>
2018-11-05 14:54:17 -0600

    Modelos

---
+++ Paqueteria/Models/Estatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paqueteria.Models
+{
+    public class Estatus
+    {
+        public int EstatusID { get; set; }
+        public string NombreSts { get; set; }
+        public string DescripcionSts { get; set; }
+
+        public ICollection<Aplicacion> Aplicaciones { get; set; }
+    }
+}
---
+++ Paqueteria/Models/Incidencia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paqueteria.Models
+{
+    public class Incidencia
+    {
+        public int IncidenciaID { get; set; }
+        [Display(Name = "Nombre")]
+        [Required]
+        [StringLength(50)]
+        public string NombreIc { get; set; }
+        [Display(Name = "Descripción")]
+        [Required]
+        public string DescripcionIc { get; set; }
+        [Display(Name = "Dependencia")]
+        [Required]
+        public int PaqueteID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha de Registro")]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha Esperada")]
+        public DateTime FinaltDate { get; set; }
+        public int EstatusID { get; set; }
+        
+        public Paquete Paquete { get; set; }
+    }
+}
